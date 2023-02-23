b64fe0049080a210fa38e25a609ecee24da70d83
Charlean Baxter <baxtercm@outlook.com>
2023-02-23 02:39:47 -0800

    added devise and links to Header.js

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,9 +1,10 @@
 import React from "react"
-
-const App = () => {
+import Header from "./components/Header"
+const App = (props) => {
   return (
     <>
       <h1>Welcome to Aunty Says</h1>
+      <Header {...props} />
     </>
   )
 }
--- app/javascript/components/components/Header.js
+++ app/javascript/components/components/Header.js
@@ -1,10 +1,37 @@
 import React from "react"
+import { Nav, NavItem } from "reactstrap"
 
-const Header = () => {
+const Header = ({
+  logged_in,
+  new_user_route,
+  sign_in_route,
+  sign_out_route
+}) => {
   return (
-    <>
-      <h3>This is the Header Page</h3>
-    </>
+    <Nav>
+      {!logged_in?
+        (<>
+          <NavItem>
+            <a href={sign_in_route} className="nav-link">
+              Sign In
+            </a>
+          </NavItem>
+          <NavItem>
+            <a href={new_user_route} className="nav-link">
+              Sign Up
+            </a>
+          </NavItem>
+        </>)
+        :
+        (<>
+          <NavItem>
+            <a href={sign_out_route} className="nav-link">
+              Sign Out
+            </a>
+          </NavItem>
+        </>)
+      } 
+    </Nav>
   )
 }
 
